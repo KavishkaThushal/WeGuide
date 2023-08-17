0b0a76372fdace0277615019f246ce6fedd37b26
Kavishka Thushal <thushalrasingolla99@gmail.com>
2023-08-17 14:41:01 +0530

    fix css part

--- frontend/src/Thanks/Thanks.jsx
+++ frontend/src/Thanks/Thanks.jsx
@@ -1,9 +1,29 @@
 import React from "react";
-import { Link, useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { Container, Row, Col } from "reactstrap";
+import "./thanks.css";
 
 function Thanks() {
-  const { id } = useParams();
-  return <div>Thanks</div>;
+  return (
+    <section>
+      <Container>
+        <Row>
+          <Col lg="12">
+            <div className="thank__you text-center mt-5 mb-5">
+              <span>
+                <i class="ri-checkbox-circle-line"></i>
+              </span>
+              <h1 className="mb-3 fw-semibold">Thank You</h1>
+              <h1 className="mb-4">Hired....</h1>
+              <button className="back_btn">
+                <Link to="/home">Back to Home</Link>
+              </button>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
 }
 
 export default Thanks;
